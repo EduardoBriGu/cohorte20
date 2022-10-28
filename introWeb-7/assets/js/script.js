23a4f75a06c9507bae60ff71984699af79faf6ec
eduardobrigu <eduardo.brionesg208@gmail>
2022-10-28 17:13:39 -0500

    introWeb-7

---
+++ introWeb-7/assets/js/script.js
@@ -0,0 +1,11 @@
+// Esto es un comentarios de una sola linea 
+
+/*  Esto es un 
+comentario de 
+un bloque */
+
+console.log("Hello world!");
+
+alert("Alerta de chisme");
+
+console.log("Esto es una suma", 5 + 5);
