19800aa158b484c484a6b597352d10812ca0666f
eduardobrigu <eduardo.brionesg208@gmail>
2022-10-31 12:20:42 -0600

    conversiones

--- introWeb-7/assets/js/script.js
+++ introWeb-7/assets/js/script.js
@@ -6,6 +6,107 @@ un bloque */
 
 console.log("Hello world!");
 
-alert("Alerta de chisme");
+//alert("Alerta de chisme");
 
 console.log("Esto es una suma", 5 + 5);
+
+
+
+
+/* METODOS DE DECLARACIONES 
+CUTRE
+var nombre;
+nombre= "Eduardo"
+
+CHIDA-
+var nombre = "Eduardo"
+*/
+
+var nombre= "Eduardo";
+var edad= 18;
+var vivo= true;
+var vivire ;
+var dinero = null;
+
+//typeof es para saber qué TIPO DE DATO que le asignamos a nuestra variable
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(vivo));
+console.log(typeof(vivire));
+console.log(typeof(dinero));
+
+//Para convertir el tipo de valor debes de poner el tipo de dato al que quieres que cambie la declaración
+//Empezando con mayusculas y luego el dato que quieras cambiar 
+
+console.log("Hola CH20");  //EJEMPLO CON STRING
+console.log(String("Hola CH20"));
+console.log(Number("Hola CH20"));
+console.log(Boolean("Hola CH20"));
+
+console.log(123456789);   //EJEMPLO CON NUMERO
+console.log(String(123456789));
+console.log(Number(123456789));
+console.log(Boolean(123456789));
+
+console.log(true);   //EJEMPLO CON BOOLEANO
+console.log(String(true));
+console.log(Number(true));
+console.log(Boolean(true));
+
+
+
+var miNacimiento = 2004;
+const añoActual = 2022;
+
+console.log(añoActual - miNacimiento);
+
+
+var nacimientoMau = 1996;
+console.log("Mau tiene ",añoActual - nacimientoMau)
+
+
+var nacimientoAna = 2000;
+console.log("Ana tiene ",añoActual - nacimientoAna)
+
+var nacimientoLeo = 2002;
+console.log("Leo tiene ",añoActual - nacimientoLeo)
+
+var nacimientoMag = 1999;
+console.log("Mag tiene", añoActual - nacimientoMag)
+
+var nacimientoEdu ;
+console.log("Eduardo Morón tiene ",añoActual - nacimientoEdu)
+
+
+
+
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+console.log(Number("Twenty"));
+console.log(String("Twenty"));
+console.log(Boolean("Twenty"));
+
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
+
+
+
